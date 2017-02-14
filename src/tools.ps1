13ea9ea93ab8f1e65a5be6ce96106a0ab3ea0996
Ed Elliott <GoEddie@users.noreply.github.com>
2017-02-14 14:28:38 +0000

    Update tools.ps1

--- src/tools.ps1
+++ src/tools.ps1
@@ -1,5 +1,5 @@
 
-$env:Path = $env:Path + "%ProgramData%\chocolatey;%ProgramData%\chocolatey\bin"
+$env:Path = $env:Path + "$($env.ProgramData)\chocolatey;$($env.ProgramData)\chocolatey\bin"
 choco install git -y
 
 choco install jenkins -y
